4c43775021a0773512f7799321545c1eb2bf6085
Mert Kirman <mertkirman@Merts-MacBook-Air.local>
2024-11-30 01:41:02 +0300

    Add ability to choose main menu options

--- src/main-screen.c
+++ src/main-screen.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-#include <time.h>
 #include <signal.h>
+#include <string.h>
 
 void enableRawMode();
 void disableRawMode();
 int kbhit();
 void handleSignal(int signal);
-void printMainMenu(char *mainMenu);
+void printMainMenu(char *mainMenu, char availableOptions[3][12]);
 
+int gameChosen = 0;
+int optionChosen = 0;
 char *mainMenu = 
 " #####   ##   ##      ##   ##        ######  #####     ##  ##            #####       ##   ##   ##   ######   ######  \n"
 "#######  ##   ##    #####  ##        ######  ######    ##  ##           #######    #####  ### ###  #######  #######  \n"
@@ -25,12 +27,15 @@ int main(){
     signal(SIGINT, handleSignal);
     signal(SIGTERM, handleSignal);
 
+    // Store available games
+    char availableGames[3][12] = {"snake", "ball", "dino"};
+
     enableRawMode();
 
     // Main menu loop
     while(1){
-        printMainMenu(mainMenu);
-        usleep(1.5E5);
+        printMainMenu(mainMenu, availableGames);
+        usleep(1E5);
 
         // Variable to store input from the user
         char input;
@@ -40,16 +45,42 @@ int main(){
             input = getchar();
 
             if(input == 'a'){
-                break;
+                optionChosen = optionChosen - 1;
+                if(optionChosen < 0){
+                    optionChosen += 3;
+                }
+                optionChosen = optionChosen % 3;
             }
             else if(input == 'd'){
-                break;
+                optionChosen = (optionChosen + 1) % 3;
             }
             else if(input == 'w'){
-                break;
+                if(optionChosen == 1) {
+                    gameChosen = gameChosen - 1;
+                    if(gameChosen < 0){
+                        gameChosen += 3;
+                    }
+                    gameChosen = gameChosen % 3;
+                }
             }
             else if(input == 's'){
-                break;
+                if(optionChosen == 1) {
+                    gameChosen = (gameChosen + 1) % 3;
+                }
+            }
+            else if(input == '\n'){
+                if(optionChosen == 0){
+                    // Prepare the command that will run the chosen game when player presses enter button
+                    char command[16] = "./game_";
+                    strcat(command, availableGames[gameChosen]);
+
+                    disableRawMode();
+                    system(command);
+                    enableRawMode();
+                }
+                else if(optionChosen == 2){
+                    break;
+                }
             }
             else if(input == 'q'){
                 break;
@@ -57,15 +88,28 @@ int main(){
         }
     }
 
-    // Restore terminal settings
-    disableRawMode();
+    system("clear");    // Clear the terminal
+    disableRawMode();   // Restore terminal settings
     return 0;
 }
 
-void printMainMenu(char *mainMenu){
+void printMainMenu(char *mainMenu, char availableGames[3][12]){
     // Clear the terminal
     system("clear");
     printf("%s", mainMenu);
+    
+    if(optionChosen == 0){
+        printf("%30s %28s %29s\n", "[start]", availableGames[gameChosen], "exit");
+    }
+    else if(optionChosen == 1){
+        char gameDisplayed[16] = "[(";
+        strcat(gameDisplayed, availableGames[gameChosen]);
+        strcat(gameDisplayed, ")]");
+        printf("%29s %31s %27s\n", "start", gameDisplayed, "exit");
+    }
+    else if(optionChosen == 2){
+        printf("%29s %29s %30s\n", "start", availableGames[gameChosen], "[exit]");
+    }
     fflush(stdout);
 }
 
@@ -97,7 +141,8 @@ int kbhit() {
 
 // Function that cleans up and exits gracefully
 void handleSignal(int signal) {
+    system("clear");    // Clear the terminal
     disableRawMode();   // Reset terminal settings
-    exit(0); // Exit the game
+    exit(0);    // Exit the console
 }
 
